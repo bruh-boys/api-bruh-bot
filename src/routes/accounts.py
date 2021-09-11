a7dc94e5d109c207bc775d9e90213e3630cec9d6
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-09-10 21:01:39 -0500

    Update accounts.py

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -5,12 +5,9 @@ import os
 load_dotenv()
 email1 = os.getenv('EMAIL1')
 email2 = os.getenv('EMAIL2')
-email3 = os.getenv('EMAIL3')
-email4 = os.getenv('EMAIL4')
-email5 = os.getenv('EMAIL5')
 password = os.getenv('PASSWORD')
 
-accounts = [email1,email2,email3,email4,email5]  # email accunts
+accounts = [email1,email2]  # email accunts
 
 p = password  # password accounts
 
